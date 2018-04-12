11d0bf633b70a6711cf745efd0e2f33f14feb544
Kennif <kennif.work@gmail.com>
2018-04-12 20:17:53 +0800

    Added the react3dphysics files and created a facade called Physics

---
+++ FPS/FPS/Physics.h
@@ -0,0 +1,13 @@
+//Physics facade encapsulating RigidBody, Raycasting and Collision using ReactPhysics3D Engine
+
+#include "react3d\reactphysics3d.h"
+
+using namespace reactphysics3d;
+
+class RigidBody
+{
+public:
+	
+private:
+	rp3d::CollisionBody world;
+};
