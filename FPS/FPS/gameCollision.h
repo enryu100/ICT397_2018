3948f3b1e4386d617d66cda9076c3c1a8d8ac9e2
Kennif <kennif.work@gmail.com>
2018-04-15 17:26:18 +0800

    gameCollision done

--- FPS/FPS/gameCollision.h
+++ FPS/FPS/gameCollision.h
@@ -4,4 +4,4 @@
 
 bool sphereColTest(gameVector2D gameObj1Centre, gameVector2D gameObj2Centre, float gameObj1Radius, float gameObj2Radius);
 void getObjectCentre(gameVector2D vertices[], int numVertices, gameVector2D &objCentre);
-void getBoundingSphereRadius(gameVector2D vertices[], int numVertices, gameVector2D objCentre);
+float getBoundingSphereRadius(gameVector2D vertices[], int numVertices, gameVector2D objCentre);
