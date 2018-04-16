ae13d77ab34a91216396699723d6ef4e8567203a
Kennif <kennif.work@gmail.com>
2018-04-16 13:33:10 +0800

    Finished documentation on almost all files

--- FPS/FPS/gameMath.h
+++ FPS/FPS/gameMath.h
@@ -3,20 +3,68 @@
 
 #include <math.h>
 
+
+//Structure which represents a point of X and Y coordinates
 typedef struct point
 { 
 	double x; 
 	double y; 
 }point2D;
 
-//Converts degrees to radians
+/**
+* @class gameMath
+* @brief A class that provides basic math functions and conversion
+*
+* This class converts different units back and forth for use in collision detection.
+*
+* Reference: ICT397 Collision Examples and ICT397 Topic06 Lecture Notes
+*
+* @author Kenneth Donohoe
+*
+*/
+
+/**
+*@brief Converts degrees to radians
+*@param double radians
+*@return double radians
+*
+* Multiplies piDiv180 with radians and returns the result
+*/
 inline double degToRad(double radians);
-//Convert from radians to degrees
+
+/**
+*@brief Converts radians to degrees
+*@param double radians
+*@return double radians
+*
+* Multiplies radians with 180 divided by PI and returns the result
+*/
 inline double radToDeg(double radians);
 
-//Converts from cartesian to polar coordinates
+/**
+*@brief Converts cartesian coordinates to polar coordinates
+*@param double x
+		double y
+*@return point polar
+*
+*/
 point cartToPolar(double x, double y);
+
+/**
+*@brief Converts polar coordinates to cartesian coordinates
+*@param point2D polar
+*@return point cart
+*
+*/
 point polarToCart(point2D polar);
 
+/**
+*@brief Moves to polar 2D co-ordinates to a specified angle and direction
+*@param double moveDistance
+		point2D cartCoordinates
+		double angle
+		int direction
+*@return No return
+*
+*/
 void movePolar2D(double moveDistance, point2D& cartCoordinates, double& angle, int direction);
-void changeSign(double &value);
