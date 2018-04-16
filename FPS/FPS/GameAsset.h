27530e559e7bd4e367614b9e08a993fc8c9e8a7a
Conean <eggcrack9@gmail.com>
2018-04-16 16:52:52 +0800

    Add files via upload
    First pass of GameAsset class

---
+++ FPS/FPS/GameAsset.h
@@ -0,0 +1,34 @@
+#ifndef _GAMEASSETFACTORY_H
+#define _GAMEASSETFACTORY_H
+
+#include <iostream>
+#include "Terrain.h"
+#include "Camera.h"
+#include "Obstacle.h"
+#include "NPC.h"
+#include "Enemy.h"
+
+using namespace std;
+
+class Asset{};
+
+class GameAsset{
+	public:
+		Asset* Create(string type);
+		
+};
+
+Asset* Create(string type){
+	if(type=="TERRAIN")
+		return new Terrain;
+	if(type=="PLAYER")
+		return new Camera;
+	if(type=="OBSTACLE")
+		return new Obstacle;
+	if(type=="NPC")
+		return new NPC;
+	if(type=="ENEMY");
+		return new Enemy;
+}
+
+#endif
