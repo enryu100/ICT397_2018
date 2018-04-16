74766a27c305dc5d02d15590c1f825f744b7bab7
Conean <eggcrack9@gmail.com>
2018-04-16 20:03:02 +0800

    Update GameAsset.h
    Doxy completed

--- FPS/FPS/GameAsset.h
+++ FPS/FPS/GameAsset.h
@@ -1,3 +1,10 @@
+/**
+*	\class GameAsset
+*	A class that creates an asset with the type
+*	specified by a string, then returning a pointer to the object
+*	@author Mitchell Holling
+*/
+
 #ifndef _GAMEASSET_H
 #define _GAMEASSET_H
 
@@ -14,6 +21,12 @@ using namespace std;
 
 class GameAsset{ 
 	public:
+		/*
+		*	\fn CreateObject
+		*	Taking in the type of asset to be created,
+		*	Creates the object, then returns a pointer to it
+		*	@param type the type of object that needs to be created
+		*/
 		GameObject* CreateObject(string type);
 		
 };
